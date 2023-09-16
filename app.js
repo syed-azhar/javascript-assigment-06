64e2c9d7f980a630967d37b8a29371c97696c885
syed-azhar <93089810+syed-azhar@users.noreply.github.com>
2023-09-15 20:46:48 -0700

    javascript-asignmnet 06

---
+++ app.js
@@ -0,0 +1,59 @@
+        // jaavascript assignment - 06
+
+// 1. Write a function that displays current date & time in your
+// browser.
+
+
+// function date(){
+//    console.log(new Date());
+// }
+// date();
+
+
+// 3. Write a function that adds two numbers (input by user)
+// and returns the sum of two numbers.
+
+
+// function sum(a,b){
+//     return (a+b)
+// }
+
+// var x = 3;
+// var y = 3;
+
+// console.log(sum(x,y))
+
+
+// 3. Write a function that adds two numbers (input by user)
+// and returns the sum of two numbers.
+
+// function sum(){
+//     var sumOfone = +prompt("enter first sum")
+//     var sumOftwo = +prompt("enter second sum")
+//     console.log(sumOfone + sumOftwo)
+// }
+// sum();
+
+
+// 4. Calculator:
+// Write a function that takes three arguments num1, num2
+// & operator & compute the desired operation. Return and
+// show the desired result in your browser.
+
+
+// function Calculator(){
+//     var num1 = 4
+//     var num2 = 3
+//     var operator = 2
+//     console.log("plus method",num1 + num2)
+//     console.log("divide method", num1 / operator)
+//     console.log("multiply method", num2 * num1)
+//     console.log("minus method", num1 - num2)
+// }
+// Calculator();
+
+
+
+
+
+
